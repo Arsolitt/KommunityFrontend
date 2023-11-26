a953340369e5184a7b436624affec1904980d9f4
Arsolitt <arsolitt@gmail.com>
2023-11-26 21:19:04 +0300

    фиксы

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,13 +1,34 @@
 const bgHeaders = document.querySelectorAll(".background__text");
+bgHeaders.forEach((bgHeader) => {
+  bgHeaderStroke(bgHeader, 1);
+});
+const pageArrows = document.querySelectorAll(".arrow");
 
 window.addEventListener("scroll", () => {
+  changeBgHeaderOpacity();
+  changeArrowPosition();
+});
+
+function changeBgHeaderOpacity() {
   bgHeaders.forEach((bgHeader) => {
     const bgHeaderHeight = bgHeader.offsetHeight;
-    const bgHeaderOffset = bgHeader.getBoundingClientRect().top + window.scrollY;
-	const currentScrollY = window.scrollY + bgHeaderHeight + 100
-    if ((window.scrollY + bgHeaderHeight + 100 >= bgHeaderOffset) && (currentScrollY <= bgHeaderOffset + bgHeaderHeight)) {
-    	const opacity = 1 - (currentScrollY - bgHeaderOffset) / bgHeaderHeight;
-		bgHeader.style.WebkitTextStroke = `1px rgba(77, 38, 129, ${opacity})`;
+    const bgHeaderOffset =
+      bgHeader.getBoundingClientRect().top + window.scrollY;
+    const currentScrollY = window.scrollY + bgHeaderHeight + 100;
+    if (
+      window.scrollY + bgHeaderHeight + 150 >= bgHeaderOffset &&
+      currentScrollY <= bgHeaderOffset + bgHeaderHeight
+    ) {
+      const opacity = 1 - (currentScrollY - bgHeaderOffset) / bgHeaderHeight;
+      bgHeaderStroke(bgHeader, opacity);
     }
   });
-});
+}
+
+function changeArrowPosition() {
+
+}
+
+function bgHeaderStroke(bgHeader, opacity) {
+  return (bgHeader.style.WebkitTextStroke = `1px rgba(43, 25, 64, ${opacity})`);
+}
