5046468b1e4dabc14e12d2ef77144e5415744b85
Arsolitt <arsolitt@gmail.com>
2023-11-26 17:36:08 +0300

    анимашки

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,49 +1,13 @@
-// const bgHeaders = document.querySelectorAll(".background__text__inner");
-
-// window.addEventListener("scroll", function () {
-//   const triggerHeader = window.innerHeight * 0.6;
-
-//   bgHeaders.forEach((bgHeader) => {
-//     const bgHeaderTop = bgHeader.getBoundingClientRect().top;
-// 	console.log('Trigger: '+triggerHeader);
-// 	console.log('Top: '+bgHeaderTop);
-// 	if (bgHeaderTop > triggerHeader) {
-// 		bgHeader.classList.remove('background__text__gradient')
-// 		bgHeader.classList.add('background__text__stroke')
-// 	} else {
-// 		bgHeader.classList.remove('background__text__stroke')
-// 		bgHeader.classList.add('background__text__gradient')
-// 	}
-//   });
-// });
-
-const header = document.querySelector(".background__text");
+const bgHeaders = document.querySelectorAll(".background__text");
 
 window.addEventListener("scroll", () => {
-  const t =
-    window.scrollY /
-    (document.documentElement.scrollHeight - window.innerHeight);
-  header.style.WebkitTextStroke = `1px rgba(77, 38, 129, ${1 - t})`;
+  bgHeaders.forEach((bgHeader) => {
+    const bgHeaderHeight = bgHeader.offsetHeight;
+    const bgHeaderOffset = bgHeader.getBoundingClientRect().top + window.scrollY;
+	const currentScrollY = window.scrollY + bgHeaderHeight + 100
+    if ((window.scrollY + bgHeaderHeight + 100 >= bgHeaderOffset) && (currentScrollY <= bgHeaderOffset + bgHeaderHeight)) {
+    	const opacity = 1 - (currentScrollY - bgHeaderOffset) / bgHeaderHeight;
+		bgHeader.style.WebkitTextStroke = `1px rgba(77, 38, 129, ${opacity})`;
+    }
+  });
 });
-// ${-t * 100}% 0
-
-// $(window).scroll(function() {
-// 	var scrollTop = $(this).scrollTop();
-
-// 	$('.background__text').css({
-// 	  opacity: function() {
-// 		var elementHeight = $(this).height();
-// 		return 1 - (elementHeight - scrollTop) / elementHeight;
-// 	  }
-// 	});
-//   });
-
-function bgHeaderStyles() {
-  return `
-	background: linear-gradient(to right, #984989, #7b7680, #7b7680);
-	background-color:rgba(0, 0, 0, 0.1);
-	background-clip: text;
-	-webkit-background-clip: text;
-	-webkit-text-stroke: 3px rgba(77, 38, 129, 1);
-	`;
-}
