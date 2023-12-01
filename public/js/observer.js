24d9c0fd272fe4bcf3b45aea8128c5cd682f1e01
Arsolitt <arsolitt@gmail.com>
2023-12-01 10:15:30 +0300

    якори

--- public/js/observer.js
+++ public/js/observer.js
@@ -16,16 +16,48 @@ drops.forEach((element) => {
 
 const arrows = document.querySelectorAll(".arrow__bounced");
 
-const arrowsObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("arrow__bounce");
-      return;
-    }
-    entry.target.classList.remove("arrow__bounce");
-  });
-});
+const arrowsObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("arrow__bounce");
+        return;
+      }
+      entry.target.classList.remove("arrow__bounce");
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
 
 arrows.forEach((element) => {
   arrowsObserver.observe(element);
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav__link");
+
+const sectionsObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => {
+          let id = link.getAttribute("href").replace("#", "");
+          if (id === entry.target.id) {
+            link.classList.add("nav__link__active");
+          } else {
+            link.classList.remove("nav__link__active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 0.35,
+  }
+);
+
+sections.forEach((element) => {
+  sectionsObserver.observe(element);
 });
