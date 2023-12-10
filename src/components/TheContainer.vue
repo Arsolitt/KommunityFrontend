fba3c640be2bf44b224eb136a2747cc7d9d7265c
Arsolitt <arsolitt@gmail.com>
2023-12-10 19:54:04 +0300

    Navbar component

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+	<div class="container"><slot /></div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+	width: 320px;
+}
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,52 @@
+<script setup></script>
+
+<template>
+	<nav>
+		<a class="logo" href=""><img alt="" src="@/assets/svg/logo.svg" /></a>
+		<ul class="menu">
+			<li class="menu__item"><a class="item__link" href="#works">Работы</a></li>
+			<li class="menu__item"><a class="item__link" href="#team">Команда</a></li>
+			<li class="menu__item">
+				<a class="item__link" href="#reviews">Отзывы</a>
+			</li>
+			<li class="menu__item"><a class="item__link" href="#shop">Магазин</a></li>
+		</ul>
+	</nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+	background-color: #fff;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 9px 0;
+	border-bottom-left-radius: 18px;
+	border-bottom-right-radius: 18px;
+}
+.logo {
+	display: inline;
+	margin-left: 16px;
+	margin-right: 32px;
+	img {
+		vertical-align: bottom;
+	}
+}
+.menu {
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	margin-right: 10px;
+	&__item {
+	}
+	.item__link {
+		font-size: 14px;
+		color: black;
+		&_active,
+		.no-touch &:hover {
+			color: #7a02b6;
+		}
+	}
+}
+</style>
