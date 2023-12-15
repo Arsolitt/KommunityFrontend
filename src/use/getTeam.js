90e590c0b92c42b51dc692a37c1fcffafccec589
Arsolitt <arsolitt@gmail.com>
2023-12-15 14:34:19 +0300

    pages

--- src/use/getTeam.js
+++ src/use/getTeam.js
@@ -4,31 +4,31 @@ export function getTeam() {
 			memberId: 0,
 			name: 'Полина',
 			specialization: 'Дизайнер',
-			img: '/assets/temp/team_1.png',
+			img: '/KommunityFrontend/temp/team_1.png',
 		},
 		{
 			memberId: 1,
 			name: 'Кузьма',
 			specialization: 'Видеомейкер',
-			img: '/assets/temp/team_2.png',
+			img: '/KommunityFrontend/temp/team_2.png',
 		},
 		{
 			memberId: 2,
 			name: 'Илья',
 			specialization: 'Арт-директор',
-			img: '/assets/temp/team_2.png',
+			img: '/KommunityFrontend/temp/team_2.png',
 		},
 		{
 			memberId: 3,
 			name: 'Егор',
 			specialization: 'Дизайнер',
-			img: '/assets/temp/team_2.png',
+			img: '/KommunityFrontend/temp/team_2.png',
 		},
 		{
 			memberId: 4,
 			name: 'Александр',
 			specialization: 'Фотограф',
-			img: '/assets/temp/team_2.png',
+			img: '/KommunityFrontend/temp/team_2.png',
 		},
 	];
 }
